859cb8e25329e2b7ebd008806d5f1c7aa51930d3
Leo <Leo>
2016-01-24 16:15:19 -0200

    fiz fullscreen issues

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ function SnakeScene(options) {
     var m_snakeColorTable;
     var m_snakeColorIndex;
     var m_snakeLastUpdatedColorTicks;
+    var m_lastScoreTicksUpdated;
     var m_nextDirectionUpdate = m_currentDirection;
     var m_score;
     var m_waitAfterReset;
@@ -36,15 +37,15 @@ function SnakeScene(options) {
         m_context.fillRect(
             m_pixelsPerDot.w * x,
             m_pixelsPerDot.h * y,
-            m_pixelsPerDot.w,
-            m_pixelsPerDot.h);
+            m_pixelsPerDot.w + 1,
+            m_pixelsPerDot.h + 1);
     };
 
     var updateFoodPosition = function () {
         m_currentFood = { x: Math.floor(Math.random() * m_dots.w), y: Math.floor(Math.random() * m_dots.h) };
     };
 
-// public:
+    // public:
 
     this.onScoreUpdate = null;
     this.onDeath = null;
@@ -71,16 +72,17 @@ function SnakeScene(options) {
         m_snakeColorTable = ["#fff", "#4af"];
         m_snakeColorIndex = 0;
         m_snakeLastUpdatedColorTicks = null;
+        m_lastScoreTicksUpdated = null;
         m_snakeWaitForChangeColorFrames = 0;
         m_nextDirectionUpdate = m_currentDirection;
         m_score = 0;
         m_waitAfterReset = 1000;
     };
 
-    this.getHeadPositionInPixels = function() {
+    this.getHeadPositionInPixels = function () {
         var l = m_currentSnake.length - 1;
-        return { 
-            x: m_currentSnake[l].x * m_pixelsPerDot.w, 
+        return {
+            x: m_currentSnake[l].x * m_pixelsPerDot.w,
             y: m_currentSnake[l].y * m_pixelsPerDot.h
         };
     };
@@ -112,85 +114,93 @@ function SnakeScene(options) {
                 if (m_waitAfterReset > 0) {
                     m_waitAfterReset -= (ticks - m_lastTicksPass);
                 }
-                else if (ticks - m_lastTicksUpdated >= m_currentFrameUpdate) {
-                    // score for turning
-                    if(m_currentDirection[0] != m_nextDirectionUpdate[0] ||
-                        m_currentDirection[1] != m_nextDirectionUpdate[1]) {
-                        m_score -= 3;
+                else {
+                    var scoreUdated = false;
+                    if (m_lastScoreTicksUpdated == null || ticks - m_lastScoreTicksUpdated >= 500) {
+                        m_score--;
+                        scoreUdated = true;
+                        m_lastScoreTicksUpdated = ticks;
                     }
-
-                    // update direction based on last keypress
-                    m_currentDirection = m_nextDirectionUpdate;
-
-
-                    // update position based on direction
-                    {
-                        if (!m_snakeIncreaseSize)
-                            m_currentSnake = m_currentSnake.slice(1);
-                        m_snakeIncreaseSize = false;
-                        var last = m_currentSnake[m_currentSnake.length - 1];
-
-                        var toPush = {
-                            x: (last.x + m_currentDirection[0]) % m_dots.w,
-                            y: (last.y + m_currentDirection[1]) % m_dots.h
+                    if (ticks - m_lastTicksUpdated >= m_currentFrameUpdate) {
+                        // score for turning
+                        if (m_currentDirection[0] != m_nextDirectionUpdate[0] ||
+                            m_currentDirection[1] != m_nextDirectionUpdate[1]) {
+                            m_score -= 3;
                         }
-                        if (toPush.x < 0) toPush.x = m_dots.w - 1;
-                        if (toPush.y < 0) toPush.y = m_dots.h - 1;
-                        m_currentSnake.push(toPush);
-                    }
 
-                    // check self colision
-                    {
-                        var last = m_currentSnake[m_currentSnake.length - 1];
-                        for (var x = 0; x < m_currentSnake.length - 1; x++) {
-                            var item = m_currentSnake[x];
-                            if (item.x == last.x && item.y == last.y) {
-                                m_gameState = GAME_STATE_DEATH;
+                        // update direction based on last keypress
+                        m_currentDirection = m_nextDirectionUpdate;
 
-                                if(this.onDeath != null)
-                                    this.onDeath(this);
+
+                        // update position based on direction
+                        {
+                            if (!m_snakeIncreaseSize)
+                                m_currentSnake = m_currentSnake.slice(1);
+                            m_snakeIncreaseSize = false;
+                            var last = m_currentSnake[m_currentSnake.length - 1];
+
+                            var toPush = {
+                                x: (last.x + m_currentDirection[0]) % m_dots.w,
+                                y: (last.y + m_currentDirection[1]) % m_dots.h
+                            }
+                            if (toPush.x < 0) toPush.x = m_dots.w - 1;
+                            if (toPush.y < 0) toPush.y = m_dots.h - 1;
+                            m_currentSnake.push(toPush);
+                        }
+
+                        // check self colision
+                        {
+                            var last = m_currentSnake[m_currentSnake.length - 1];
+                            for (var x = 0; x < m_currentSnake.length - 1; x++) {
+                                var item = m_currentSnake[x];
+                                if (item.x == last.x && item.y == last.y) {
+                                    m_gameState = GAME_STATE_DEATH;
+
+                                    if (this.onDeath != null)
+                                        this.onDeath(this);
+                                }
                             }
                         }
+
+                        // check for get food
+                        {
+                            var last = m_currentSnake[m_currentSnake.length - 1];
+                            if (last.x == m_currentFood.x && last.y == m_currentFood.y) {
+                                // found
+                                // push new food
+                                updateFoodPosition();
+
+                                // increase snake size by one
+                                m_snakeIncreaseSize = true;
+
+                                // update "update rate"
+                                // get 10% more closer to maximum update rate
+                                m_currentFrameUpdate -= (m_currentFrameUpdate - m_maximumUpdateRate) * 0.1;
+
+                                // add a score
+                                m_score += 100;
+
+                                if (this.onEatFood != null)
+                                    this.onEatFood(this);
+
+                                m_snakeColorIndex = 1;
+                                m_snakeLastUpdatedColorTicks = ticks;
+                            }
+                        }
+
+                        scoreUdated = true;
+
+                        // limit bottom score
+
+                        m_lastTicksUpdated = ticks;
+                        m_waitForNextUpdate = false;
                     }
-
-                    // check for get food
-                    {
-                        var last = m_currentSnake[m_currentSnake.length - 1];
-                        if (last.x == m_currentFood.x && last.y == m_currentFood.y) {
-                            // found
-                            // push new food
-                            updateFoodPosition();
-
-                            // increase snake size by one
-                            m_snakeIncreaseSize = true;
-
-                            // update "update rate"
-                            // get 10% more closer to maximum update rate
-                            m_currentFrameUpdate -= (m_currentFrameUpdate - m_maximumUpdateRate) * 0.1;
-
-                            // add a score
-                            m_score += 100;
-
-                            if (this.onEatFood != null)
-                                this.onEatFood(this);
-
-                            m_snakeColorIndex = 1;
-                            m_snakeLastUpdatedColorTicks = ticks;
+                    if (scoreUdated) {
+                        m_score = Math.max(m_score, 0);
+                        if (this.onScoreUpdate != null) {
+                            this.onScoreUpdate(this);
                         }
                     }
-
-                    // update score
-                    {
-                        m_score = Math.max(0, m_score - 1);
-                    }
-
-                    // limit bottom score
-
-                    if (this.onScoreUpdate != null)
-                        this.onScoreUpdate(this);
-
-                    m_lastTicksUpdated = ticks;
-                    m_waitForNextUpdate = false;
                 }
                 break;
         }
@@ -231,7 +241,7 @@ function SnakeScene(options) {
         }
     };
 
-// init:
+    // init:
     this.reset();
 }
 
